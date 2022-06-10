659a0ed9b2b7c4a601bc6951e88c53673ff53f18
Stefan Froemken <froemken@gmail.com>
2022-06-10 12:43:20 +0200

    Do not check for search call since TYPO3 11.3 in EventListener

--- Classes/EventListener/GeneratePublicUrlForResourceEventListener.php
+++ Classes/EventListener/GeneratePublicUrlForResourceEventListener.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 namespace JWeiland\Bynder2\EventListener;
 
 use JWeiland\Bynder2\Driver\BynderDriver;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Resource\Event\GeneratePublicUrlForResourceEvent;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Filelist\FileFacade;
 
 class GeneratePublicUrlForResourceEventListener
@@ -24,8 +26,20 @@ class GeneratePublicUrlForResourceEventListener
             return;
         }
 
-        $publicUrl = '';
-        if (!$this->isSearchCall()) {
+        // Customer has searched for keywords which results in thousands of files. As we currently do not cache the
+        // publicUrl, rendering of the file list needs ages, because for each file the Bynder API has to be called
+        // to get the public URL. Set publicUrl to empty string here, will prevent Fluid from rendering
+        // the public URL. See EXT:filelist/Resources/Private/Templates/FileList/Search.html.
+
+        // Since TYPO3 11.3 the FileListController gets a huge refactoring. No more Fluid rendering. So,
+        // setting publicUrl to empty string is not needed anymore.
+        $typo3Branch = GeneralUtility::makeInstance(Typo3Version::class)->getBranch();
+        if (
+            version_compare($typo3Branch, '11.3', '<')
+            && $this->isSearchCall()
+        ) {
+            $publicUrl = '';
+        } else {
             $publicUrl = $bynderDriver->getBynderService()->getCdnDownloadUrl(
                 $event->getResource()->getIdentifier()
             );
@@ -43,7 +57,7 @@ class GeneratePublicUrlForResourceEventListener
      */
     protected function isSearchCall(): bool
     {
-        $backTrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 7);
+        $backTrace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 10);
         foreach ($backTrace as $entry) {
             if ($entry['class'] === FileFacade::class && $entry['function'] === 'getPublicUrl') {
                 return true;
